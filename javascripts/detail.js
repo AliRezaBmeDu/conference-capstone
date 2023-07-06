c7aa39bfbefb5151bc51aa6105e61b88228ce41c
AliReza <ali.reza.buet@gmail.com>
2023-07-07 02:07:34 +0600

    Fix linter errors

--- javascripts/detail.js
+++ javascripts/detail.js
@@ -1,42 +1,42 @@
 // Create speakers cards
 const mobileSpeakers = [
-    {
-      imageSrc: './images/speakers/Jim Simons.jpg',
-      name: 'Jim Simons',
-      role: 'Mathematician and founder of Renaissance Tech',
-      description: 'Known for quantitative hedge funding'
-    },
-    {
-      imageSrc: './images/speakers/Ray Dalio.jpg',
-      name: 'Ray Dalio',
-      role: 'Founder, Bridgewater Associates',
-      description: 'Renowned hedge fund manager and author'
-    },
-    {
-      imageSrc: './images/speakers/Mary Erdoes.jpg',
-      name: 'Mary Erdoes',
-      role: 'CEO, J.P. Morgan',
-      description: 'Recognized for her vision and client-centric approach'
-    },
-    {
-      imageSrc: './images/speakers/Carl Icahn.png',
-      name: 'Carl Icahn',
-      role: 'Chairman, Icahn Enterprises',
-      description: 'Known for his aggressive investment strategies'
-    },
-    {
-      imageSrc: './images/speakers/Larry Fink.jpg',
-      name: 'Larry Fink',
-      role: 'CEO of BlackRock',
-      description: 'Known for advocating sustainable investing'
-    },
-    {
-      imageSrc: './images/speakers/Ken Griffin.jpg',
-      name: 'Ken Griffin',
-      role: 'Founder CEO of Citadel',
-      description: 'Known for his quantitative trading strategies'
-    }
-  ];
+  {
+    imageSrc: './images/speakers/Jim Simons.jpg',
+    name: 'Jim Simons',
+    role: 'Mathematician and founder of Renaissance Tech',
+    description: 'Known for quantitative hedge funding',
+  },
+  {
+    imageSrc: './images/speakers/Ray Dalio.jpg',
+    name: 'Ray Dalio',
+    role: 'Founder, Bridgewater Associates',
+    description: 'Renowned hedge fund manager and author',
+  },
+  {
+    imageSrc: './images/speakers/Mary Erdoes.jpg',
+    name: 'Mary Erdoes',
+    role: 'CEO, J.P. Morgan',
+    description: 'Recognized for her vision and client-centric approach',
+  },
+  {
+    imageSrc: './images/speakers/Carl Icahn.png',
+    name: 'Carl Icahn',
+    role: 'Chairman, Icahn Enterprises',
+    description: 'Known for his aggressive investment strategies',
+  },
+  {
+    imageSrc: './images/speakers/Larry Fink.jpg',
+    name: 'Larry Fink',
+    role: 'CEO of BlackRock',
+    description: 'Known for advocating sustainable investing',
+  },
+  {
+    imageSrc: './images/speakers/Ken Griffin.jpg',
+    name: 'Ken Griffin',
+    role: 'Founder CEO of Citadel',
+    description: 'Known for his quantitative trading strategies',
+  },
+];
 
 // Create the first section for mobile
 const mobileSection = document.getElementById('speaker-mobile');
@@ -49,9 +49,7 @@ mobileSection.appendChild(mobileHeading);
 const mobileSpeakerContainer = document.createElement('div');
 mobileSpeakerContainer.className = 'speaker-col';
 
-
-
-mobileSpeakers.forEach(speaker => {
+mobileSpeakers.forEach((speaker) => {
   const card = document.createElement('div');
   card.className = 'card';
 
@@ -95,7 +93,7 @@ desktopSpeakerRow.className = 'speaker-row row';
 // Create speaker cards for desktop section
 const desktopSpeakers = mobileSpeakers; // Use the same speakers as in the mobile section
 
-desktopSpeakers.forEach(speaker => {
+desktopSpeakers.forEach((speaker) => {
   const col = document.createElement('div');
   col.className = 'col-6';
 
@@ -128,5 +126,3 @@ desktopSpeakers.forEach(speaker => {
 });
 
 desktopSection.appendChild(desktopSpeakerRow);
-
-
--- javascripts/menu-button.js
+++ javascripts/menu-button.js
@@ -4,19 +4,19 @@ const menuPage = document.querySelector('.mobile-menu');
 const closeButton = document.querySelector('.close-button');
 
 menuButton.addEventListener('click', () => {
-    console.log("Hello, it's being called");
-    otherComponents.forEach((subelements) => {
-      subelements.classList.add('hide-seek');
-    });
-    menuPage.style.display = 'block';
-    menuButton.classList.add('hide-seek');
+  console.log("Hello, it's being called");
+  otherComponents.forEach((subelements) => {
+    subelements.classList.add('hide-seek');
   });
+  menuPage.style.display = 'block';
+  menuButton.classList.add('hide-seek');
+});
 
-  closeButton.addEventListener('click', () => {
-    console.log('Hello, closebutton is being called');
-    menuPage.style.display = 'none';
-    menuButton.classList.remove('hide-seek');
-    otherComponents.forEach((subelements) => {
-      subelements.classList.remove('hide-seek');
-    });
+closeButton.addEventListener('click', () => {
+  console.log('Hello, closebutton is being called');
+  menuPage.style.display = 'none';
+  menuButton.classList.remove('hide-seek');
+  otherComponents.forEach((subelements) => {
+    subelements.classList.remove('hide-seek');
   });
+});
